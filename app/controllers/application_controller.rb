3bf2e5524c50bc8136147ce4e6c9a4493dedad48
Aaron <a.akuzum@gmail.com>
2020-06-22 22:56:33 -0700

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,11 +24,29 @@ class User < ApplicationRecord
 
     attr_reader :password
     after_initialize :ensure_session_token
-    
+
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
         user && user.is_password?(password) ? user : nil
     end
 
+    def password(password)
+        @password = password
+        self.password_digest = Bcrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        Bcrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token        
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
