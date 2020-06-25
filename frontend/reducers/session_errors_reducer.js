d1d09a6e63a71da568742089500185dbc3cea906
Aaron <a.akuzum@gmail.com>
2020-06-24 18:52:04 -0700

    auth

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_USER } from '../actions/session_actions'
+import { 
+    RECEIVE_SESSION_ERROR, 
+    REMOVE_SESSION_ERRORS 
+    } from '../actions/error_actions'
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case RECEIVE_USER:
+            return []
+        case REMOVE_SESSION_ERRORS:
+            return []
+        case RECEIVE_SESSION_ERROR:
+            return action.errors
+        default:
+            return state
+    }
+}
+
