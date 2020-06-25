e0a97ba4e9ef009eab2b1d203ad703d0510773cc
Aaron <a.akuzum@gmail.com>
2020-06-24 21:08:39 -0700

    auth

---
+++ frontend/components/modal/modal.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { closeModal } from '../../actions/modal_actions';
+import { connect } from 'react-redux';
+import SignupFormContainer from '../session/signup_form_container'
+import LoginFormContainer from '../session/login_form_container'
+import LogoutFormContainer from '../session/logout_form_container'
+
+const Modal = ({ modal, closeModal }) => {
+    if (!modal) return null
+    let component
+    let modalClass
+    let modalChildClass
+
+    switch (modal) {
+        case 'login':
+          component = <LoginFormContainer />;
+          modalClass = 'modal-background';
+          modalChildClass = 'modal-child';
+          break;
+        case 'signup':
+          component = <SignupFormContainer />;
+          break;
+        case 'logout':
+        component = <LogoutFormContainer />;
+        break;
+        default:
+          return null;
+      }
+
+      return (
+        <div className={modalClass} onClick={closeModal}>
+          <div className={modalChildClass} onClick={e => e.stopPropagation()}>
+            { component }
+          </div>
+        </div>
+      );
+    }
+    
+    const mapStateToProps = state => {
+      return {
+        modal: state.ui.modal
+      };
+    };
+    
+    const mapDispatchToProps = dispatch => {
+      return {
+        closeModal: () => dispatch(closeModal())
+      };
+    };
+    
+    export default connect(mapStateToProps, mapDispatchToProps)(Modal);
