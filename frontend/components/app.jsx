e1f69ff5da28a5aa7cec8a08dc5a6474bc5c798c
Aaron <a.akuzum@gmail.com>
2020-08-07 22:32:41 -0700

    adds

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-
 import SignupFormContainer from './session/signup_form_container';
 import LoginFormContainer from './session/login_form_container';
 import Splash from './splash';
@@ -12,7 +11,7 @@ const App = () => {
   return (
     <div>
       {/* <Modal /> */}
-      
+
       <Switch>
         <AuthRoute exact path='/signup' component={SignupFormContainer} />
         <AuthRoute exact path='/login' component={LoginFormContainer} />
